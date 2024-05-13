8481b2d10fd31c0b489e9e8e91b847917ec15b30
Shikha Dhiman <dshikha4403@gmail.com>
2024-05-13 23:50:21 +0530

    modified core and associate page

---
+++ public/JS/core.js
@@ -0,0 +1,73 @@
+// clicking log out button
+const logout = document.getElementById("log-in-out");
+logout.addEventListener("click", function () {
+  alert("Logged out!");
+});
+
+// =======================================================================
+// Dummy data for notices
+const notices = [
+    {
+        sno: 1,
+        title: "Title 1",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendinumquam ea ducimus nemo voluptates totam",
+      },
+      {
+        sno: 2,
+        title: "Title 2",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendinumquam ea ducimus nemo voluptates totam",
+      },
+      {
+        sno: 3,
+        title: "Title 3",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendinumquam ea ducimus nemo voluptates totam",
+      },
+      {
+        sno: 4,
+        title: "Title 4",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendinumquam ea ducimus nemo voluptates totam",
+      }
+    ]
+// adding notices to notice list in 'Notice Board'
+notices.forEach(row => {
+    document.getElementById("notice-list").innerHTML += `<li>
+    <h5 class="notice-title">${row.title}</h5>
+    ${row.description}
+  </li>
+  <hr />`
+})
+
+
+// ======================================================================================
+// Adding 
+const addNotice = document.getElementById("addNotice");
+addNotice.addEventListener("click", function () {
+  const noticeTitle = document.getElementById("notice-title").value;
+  const noticeDescription = document.getElementById("notice-desc").value;
+
+  // dummy data
+  const noticeList = [{sno:1, title:noticeTitle, description:noticeDescription}]
+  
+  noticeList.forEach(row => {
+    document.getElementById("notice-list").innerHTML += `<li>
+    <h5 class="notice-title">${row.title}</h5>
+    ${row.noticeDescription}
+  </li>`;
+  })
+});
+
+
+// Or use function to add notice
+function appendNotice(notices){
+    notices.forEach(row => {
+        document.getElementById("notice-list").innerHTML += `<li>
+        <h5 class="notice-title">${row.title}</h5>
+        ${row.description}
+      </li>
+      <hr />`
+    })
+}
